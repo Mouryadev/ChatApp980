94865bb66c2af9b71d7460aa9219855c2c9701ed
Mourya Dev <mouryapooja980@gmail.com>
2025-09-20 14:45:28 +0530

    update css

--- chat-app/backend/index.js
+++ chat-app/backend/index.js
@@ -15,8 +15,13 @@ app.use(cors());
 app.use(express.json());
 
 // MongoDB Connection
-mongoose.connect('mongodb://localhost:27017/chat-app', { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => console.log('MongoDB connected'))
+const MONGO_URI =
+  "mongodb+srv://mouryapooja980:Mourya980%40@chatapp.vuwmqvf.mongodb.net/chat-app?retryWrites=true&w=majority";
+mongoose
+  .connect(MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })  .then(() => console.log('MongoDB connected'))
   .catch(err => console.log(err));
 
 // User Schema
@@ -76,7 +81,7 @@ app.post('/api/signin', async (req, res) => {
 // Get all users
 app.get('/api/users', authenticateToken, async (req, res) => {
   const users = await User.find({}, 'username').lean();
-  res.json(users.filter(user => user._id.toString() !== req.user.id));
+  res.json(users.filter(user => user._id.toString() !== req.user.id));  
 });
 
 // Get messages between two users
